af266aae97357ad535d6998549feed8ed5f85449
Nicolas Bolañez <Zven94>
2023-11-22 19:01:58 -0300

    update index style

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import HomePage from './components/HomePage/Homepage';
+// import HomePage from './components/HomePage/HomePage';
+import HomePage from './components/HomePage/HomePage';
+import AboutPage from './components/AboutPage/AboutPage';
+import ProjectsPage from './components/ProjectsPage/ProjectsPage';
 import reportWebVitals from './reportWebVitals';
 
 const BrowserRouter = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/about',
+    element: <AboutPage />,
+  },
+  {
+    path: '/projects',
+    element: <ProjectsPage />,
   },
 ]);
 
