02d62ff4cc4a031f76f3211fe3ab7fbc3adfca60
Nicolas Bolañez <Zven94>
2023-11-30 18:04:07 -0300

    update social page with links to social medias and style

--- src/components/SocialNav/SocialNav.js
+++ src/components/SocialNav/SocialNav.js
@@ -7,9 +7,18 @@ function SocialNav() {
   return (
     <>
       <div className="d-flex flex-column justify-content-around align-items-center socialNavDiv">
-        <FaGithub />
-        <FaXTwitter />
-        <FaLinkedin />
+        <a href="https://github.com/Zven94" target="_blank" rel="noreferrer" className="aSocialMedia">
+          <div className="aSocialMediaText">|</div>
+          <FaGithub />
+        </a>
+        <a href="https://github.com/Zven94" target="_blank" rel="noreferrer" className="aSocialMedia">
+          <div className="aSocialMediaText">|</div>
+          <FaXTwitter />
+        </a>
+        <a href="https://www.linkedin.com/in/nicolas-emiliano/" target="_blank" rel="noreferrer" className="aSocialMedia">
+          <div className="aSocialMediaText">|</div>
+          <FaLinkedin />
+        </a>
       </div>
     </>
   );
