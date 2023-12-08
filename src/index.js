02c192aa0650924a8dac2624a6d5e401f5f72e39
Nicolas Bolañez <Zven94>
2023-12-07 22:20:02 -0300

    updated index.js file to deploy

--- src/index.js
+++ src/index.js
@@ -2,33 +2,18 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import HomePage from './components/HomePage/HomePage';
-// import AboutPage from './components/AboutPage/AboutPage';
-// import ProjectsPage from './components/ProjectsPage/ProjectsPage';
-import reportWebVitals from './reportWebVitals';
-
-const BrowserRouter = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-  // {
-  //   path: '/about',
-  //   element: <AboutPage />,
-  // },
-  // {
-  //   path: '/projects',
-  //   element: <ProjectsPage />,
-  // },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NavBar />
-    <RouterProvider router={BrowserRouter} />
+    <HashRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>,
 );
-reportWebVitals();
